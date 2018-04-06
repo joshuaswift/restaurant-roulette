c693d3c85238de9a19494ddb95bd115ed01c006c
Joshua Swift <joshuaswift@outlook.com>
2018-04-06 07:28:12 +0100

    Added random functionality

---
+++ src/components/RestaurantButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "../css/style.css";
+
+class RestaurantButton extends Component {
+  render() {
+    return {};
+  }
+}
+
+export default RestaurantButton;
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,52 @@
-const path = require('path');
+const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 //Define paths
 const paths = {
-    DIST: path.resolve(__dirname, 'dist'),
-    JS: path.resolve(__dirname, 'src/js'),
-    SRC: path.resolve(__dirname, 'src')
+  DIST: path.resolve(__dirname, "dist"),
+  JS: path.resolve(__dirname, "src/js"),
+  SRC: path.resolve(__dirname, "src")
 };
 
 //Webpack config
 module.exports = {
-    entry: path.join(paths.JS, 'app.js'),
-    output: {
-        path: paths.DIST,
-        filename: 'app.bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(paths.SRC, 'index.html'),
-        }),
-        new ExtractTextPlugin('style.bundle.css')
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: [
-                    'babel-loader'
-                ]
-            },
-            //CSS loader for CSS files
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({
-                    use: 'css-loader'
-                })
-            },
-            //File loader for image assets
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    'file-loader',
-                ]
-            }
-        ]
-    },
-    //Enable importing JS files without specifying file extension
-    resolve: {
-        extensions: ['.js', '.jsx']
-    }
+  entry: path.join(paths.JS, "app.js"),
+  output: {
+    path: paths.DIST,
+    filename: "app.bundle.js"
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, "index.html")
+    }),
+    new ExtractTextPlugin("style.bundle.css")
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      //CSS loader for CSS files
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: "css-loader"
+        })
+      },
+      //File loader for image assets
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  //Enable importing JS files without specifying file extension
+  resolve: {
+    extensions: [".js", ".jsx"]
+  }
 };
