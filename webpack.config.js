732bccc9efe203d1e3fc731eee21f85ffb2bf5c4
Joshua Swift <joshuaswift@outlook.com>
2018-04-11 20:22:11 +0100

    start script

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const port = process.env.PORT || 5000;
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
