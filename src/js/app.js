192fe2541544699db367a59200c7c71e1e3559b5
Joshua Swift <joshuaswift@outlook.com>
2018-06-04 18:45:12 +0100

    Fixed whitespace issue on mobile browsers

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import "../css/flex-styles.css";
 import "../css/background-image.css";
+import "../css/style.css";
 
 import Header from "../components/Header";
 import RestaurantContainer from "../components/RestaurantContainer";
